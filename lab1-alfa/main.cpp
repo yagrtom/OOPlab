d4fe45f72e32bd6f4d0664cab969563d01c40e7d
yagrtom <160411694+yagrtom@users.noreply.github.com>
2024-05-17 21:43:37 +0300

    Add files via upload

--- lab1-alfa/main.cpp
+++ lab1-alfa/main.cpp
@@ -2,6 +2,8 @@
 #include <time.h>
 using namespace std;
 #include <algorithm>
+#include <fstream>
+
 int const NMAX = 500000;
 void RandomArray(int arr[], int N)
 {
@@ -26,20 +28,20 @@ void SwapElem(int* a, int* b)
     *a = *b;
     *b = k;
 }
-void sortN2(int arr[], int N)
-{
-    int k;
-    for (size_t i = 0; i < N; i++)
+    void sortN2(int arr[], int N)
     {
-        for (size_t j = 0; j < N-1; j++)
+        int k;
+        for (size_t i = 0; i < N; i++)
         {
-            if (arr[j] >= arr[j+1])
+            for (size_t j = 0; j < N-1; j++)
             {
-                SwapElem(&arr[j], &arr[j+1]);
-            }
-        }  
+                if (arr[j] >= arr[j+1])
+                {
+                    SwapElem(&arr[j], &arr[j+1]);
+                }
+            }  
+        }
     }
-}
 void CopyArray(int arr[], int arrcopy[], int N)
 {
     for (size_t i = 0; i < N; i++)
@@ -235,8 +237,17 @@ void Collectmean3(int arr[], int N)
     Secundomerstdsort(arr, N, 10);
     cout << "\n------------------------------------------------------------------------------------------------------------" << endl;
 }
-void CollectMeanForGraf()
+void CollectMeanForTable()
 {
+/*
+    std::ofstream out;			// поток для записи
+	out.open("hello.txt"); 		// открываем файл для записи
+	if (out.is_open())
+	{
+		out << "Hello World!" << std::endl;
+	}
+	out.close(); 
+*/
     for (size_t N = 100; N < NMAX; N = N + 100)
     {
         cout << "N = " << N << " :" << endl;
@@ -247,10 +258,113 @@ void CollectMeanForGraf()
         Collectmean3(arr, N);
         delete arr;
     }
-    
+}
+
+
+
+
+void SecundomerSortN2TXT(int arr[],int N, int repead)
+{
+    std::ofstream minSortN2;			// поток для записи
+    minSortN2.open("minSortN2.txt", std::ios_base::app); 
+    std::ofstream maxSortN2;			// поток для записи
+    maxSortN2.open("maxSortN2.txt", std::ios_base::app); 
+    std::ofstream halfSortN2;			// поток для записи
+    halfSortN2.open("halfSortN2.txt", std::ios_base::app); 
+    double *miliseconds = new double[repead];
+    for (size_t i = 0; i < repead; i++)
+    {
+        int *arr2 = new int[N];
+        CopyArray(arr, arr2, N);
+        clock_t start = clock();
+            sortN2(arr2, N);
+        clock_t end = clock();
+        miliseconds[i] = ((double)(end - start)); 
+        delete arr2;
+    }
+    double *meaning = new double[3];
+    meaning[0] = MIN(miliseconds, repead);
+    meaning[1] = MAX(miliseconds, repead);   
+    meaning[2] = HALF(miliseconds, repead);
+        minSortN2 << meaning[0] << endl;
+        maxSortN2 << meaning[1] << endl;
+        halfSortN2 << meaning[2] << endl;
+           
+}
+void SecundomerSortlogTXT(int arr[],int N, int repead)
+{
+    std::ofstream minSortlog;			// поток для записи
+    minSortlog.open("minSortlog.txt", std::ios_base::app); 
+    std::ofstream maxSortlog;			// поток для записи
+    maxSortlog.open("maxSortlog.txt", std::ios_base::app); 
+    std::ofstream halfSortlog;			// поток для записи
+    halfSortlog.open("halfSortlog.txt", std::ios_base::app); 
+    double *miliseconds = new double[repead];
+    for (size_t i = 0; i < repead; i++)
+    {
+        int *arr2 = new int[N];
+        CopyArray(arr, arr2, N);
+        clock_t start = clock();
+            sortlog(arr2, N);
+        clock_t end = clock();
+        miliseconds[i] = ((double)(end - start));
+        delete arr2;
+    }
+    double *meaning = new double[3];
+    meaning[0] = MIN(miliseconds, repead);
+    meaning[1] = MAX(miliseconds, repead);   
+    meaning[2] = HALF(miliseconds, repead);
+        minSortlog << meaning[0] << endl;
+        maxSortlog << meaning[1] << endl;
+        halfSortlog << meaning[2] << endl;    
+}
+void SecundomerstdsortTXT(int arr[],int N, int repead)
+{
+    std::ofstream minstdsort;			// поток для записи
+    minstdsort.open("minstdsort.txt", std::ios_base::app); 
+    std::ofstream maxstdsort;			// поток для записи
+    maxstdsort.open("maxstdsort.txt", std::ios_base::app); 
+    std::ofstream halfstdsort;			// поток для записи
+    halfstdsort.open("halfstdsort.txt", std::ios_base::app); 
+    double *miliseconds = new double[repead];
+    for (size_t i = 0; i < repead; i++)
+    {
+        int *arr2 = new int[N];
+        CopyArray(arr, arr2, N);
+        clock_t start = clock();
+            std::sort(arr,arr + N);
+        clock_t end = clock();
+        miliseconds[i] = ((double)(end - start));
+        delete arr2;
+    }
+    double *meaning = new double[3];
+    meaning[0] = MIN(miliseconds, repead);
+    meaning[1] = MAX(miliseconds, repead);   
+    meaning[2] = HALF(miliseconds, repead);
+        minstdsort << meaning[0] << endl;
+        maxstdsort << meaning[1] << endl;
+        halfstdsort << meaning[2] << endl;
+   
+}
+void CollectMeanForTableTXT()
+{
+    std::ofstream Ntxt;			// поток для записи
+    Ntxt.open("Ntxt.txt", std::ios_base::app); 
+    for (size_t N = 450000; N < NMAX; N = N + 50000)
+    {
+        cout << "N = " << N << " :" << endl;
+        int *arr = new int[N];
+        RandomArray(arr, N);
+        SecundomerSortN2TXT(arr, N, 10);
+        SecundomerSortlogTXT(arr, N, 10);
+        SecundomerstdsortTXT(arr, N, 10);
+        Ntxt << N << endl;
+        delete arr;
+    }
 }
 int main()
 {
-    CollectMeanForGraf();
+    CollectMeanForTableTXT();
+
     return 0;
 }
