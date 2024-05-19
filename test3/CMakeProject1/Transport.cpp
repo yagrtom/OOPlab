0a506c19db30064ae97d25f6947ddda09c6f1209
yagrtom <160411694+yagrtom@users.noreply.github.com>
2024-05-19 22:13:02 +0300

    Delete test3/CMakeProject1 directory

--- test3/CMakeProject1/Transport.cpp
+++
@@ -1,7 +0,0 @@
-#include "Transport.hpp"
-void Transport::MyMove(double sizeX, double sizeY, double sizeZ)
-{
-	cout << "\n Transport Move to: " << endl;
-	this->SettestCreateZero();
-	this->SetNewCoord(sizeX, sizeY, sizeZ);
-}
